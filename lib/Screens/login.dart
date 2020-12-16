f12f2613cce44002f66c563c9a48c99ec21d9940
Badari Maddali <badri.maddali@gmail.com>
2020-12-16 13:59:34 +0530

    first commit

---
+++ lib/Screens/login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  Widget _emailTextField() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      decoration: boxDecorationStyle,
+
+    )
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
